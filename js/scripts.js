7cad2bb03b87f3b48967fd557cdca2f8801847b0
Arthur Erpen <77343288+artherp@users.noreply.github.com>
2024-08-08 20:47:24 -0300

    modularizando o modal popup

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,9 @@
+import { Modal } from './modal.js'
+
 //variaveis
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-
-const Modal = {
-
-    wrapper: document.querySelector('.modal-wrapper'),
-    message: document.querySelector('.modal .title span'),
-    buttonClose: document.querySelector('.modal button.close'),
-
-    open() {
-        Modal.wrapper.classList.add('open')
-    },
-    close() {
-        Modal.wrapper.classList.remove('open')
-    }
-}
 
 form.onsubmit = function(event) {
     event.preventDefault()
@@ -28,10 +16,6 @@ form.onsubmit = function(event) {
 
     Modal.message.innerText = message
     Modal.open()
-}
-
-Modal.buttonClose.onclick = () => {
-    Modal.close()
 }
 
 function IMC(weight, height) {
