53552d5707338ba3b626656f8f141278e7db0a77
Arthur Erpen <77343288+artherp@users.noreply.github.com>
2024-08-08 20:25:39 -0300

    estruturando dados do popup

--- scripts.js
+++ scripts.js
@@ -3,9 +3,19 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessagem = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
+const Modal = {
+
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
+
+    open() {
+        Modal.wrapper.classList.add('open')
+    },
+    close() {
+        Modal.wrapper.classList.remove('open')
+    }
+}
 
 form.onsubmit = function(event) {
     event.preventDefault()
@@ -16,12 +26,12 @@ form.onsubmit = function(event) {
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
 
-    modalMessagem.innerText = message
-    modalWrapper.classList.add('open')
+    Modal.message.innerText = message
+    Modal.open()
 }
 
-modalBtnClose.onclick = () => {
-    modalWrapper.classList.remove('open')
+Modal.buttonClose.onclick = () => {
+    Modal.close()
 }
 
 function IMC(weight, height) {
