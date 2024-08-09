07892a07553658832b28f016d679d6ab1616c0ec
Arthur Erpen <77343288+artherp@users.noreply.github.com>
2024-08-08 21:20:46 -0300

    refaturando a função de submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 import { Modal } from './modal.js'
 import { AlertError } from "./alert-error.js"
-import { IMC, notNumber } from "./utils.js"
+import { calculateIMC, notNumber } from "./utils.js"
 
-//variaveis
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
@@ -13,16 +12,20 @@ form.onsubmit = function(event) {
     const weight = inputWeight.value
     const height = inputHeight.value
 
-    const showAlertError = notNumber(weight) || notNumber(height)
+    const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height)
 
-    if (showAlertError) {
+    if (weightOrHeightIsNotANumber) {
         AlertError.open()
         return;
     }
 
     AlertError.close()
 
-    const result = IMC(weight, height)
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
     const message = `Seu IMC é de ${result}`
 
     Modal.message.innerText = message
